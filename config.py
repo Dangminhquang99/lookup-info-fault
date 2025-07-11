78eea048bafe8bd9eb8cdf7070d36fb19eb46596
Dangminhquang99 <Minhquang7274@gmail.com>
2025-07-11 11:05:08 +0700

    Update config.py

--- config.py
+++ config.py
@@ -5,8 +5,8 @@ import openpyxl as op
 #============== Truy xuất file lũy kế để tính toán =======================
 def accum():
     tendz_map = {
-        "Đường dây 273 KrongBuk - 271 Nha Trang": ["Krb-nt.xlsx", "Tongke_krb.xlsx"],
-        "Đường dây 272 Thiên Tân - 271 Cam Ranh": ["Dn.xlsx", "Tongke_dn.xlsx"]
+        "Đường dây 273 KrongBuk - 271 Nha Trang": ["krb-nt.xlsx", "Tongke_krb.xlsx"],
+        "Đường dây 272 Thiên Tân - 271 Cam Ranh": ["dn.xlsx", "Tongke_dn.xlsx"]
     }
 
     choose_dz = st.selectbox("📂 Hãy chọn đường dây", list(tendz_map.keys()))
