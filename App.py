c779f7a1368d7f64847eeb2c7dbaaf02e9aefd9a
Dangminhquang99 <Minhquang7274@gmail.com>
2025-07-13 18:37:18 +0700

    Update App.py

--- App.py
+++ App.py
@@ -10,12 +10,16 @@ st.title("📊 TRA CỨU SỰ CỐ" )
 
 #Cấu hình đường dây
 select_dz=gf.select_name_dz()
+xem_tongke=st.checkbox("Xem tổng kê đường dây")
 tba_0, tba_1=gf.select_tba_1(select_dz)
 df = gf.accum(select_dz)
+if xem_tongke:
+    df
 col1, col2=st.columns(2)
+
 with col1:  #TẠI TBA 1 #cột 1
     col_name=gf.ass_col_name(tba_0)
-    st.header(f"{tba_0}")
+    st.header(f"📋-----{tba_0}------")
     st.markdown("---")
     dis87_1 = st.number_input(f"🔢 Nhập khoảng cách sự cố F87/1:", min_value=0)
     dis21_1 = st.number_input(f"🔢 Nhập khoảng cách sự cố F21/1:", min_value=0)
@@ -25,10 +29,10 @@ with col1:  #TẠI TBA 1 #cột 1
     result_21 = al.findx(dis21_1, df, "F21", col_name)
     if result_21:
         al.info(df, result_21, "F21")
-        
+    
 with col2:#TẠI TBA 2 #cột 2
     col_name=gf.ass_col_name(tba_1)
-    st.header(f"{tba_1}")
+    st.header(f"📋-----{tba_1}------")
     st.markdown("---")
     dis87_2 = st.number_input(f"🔢 Nhập khoảng cách sự cố F87/2:", min_value=0)
     dis21_2 = st.number_input(f"🔢 Nhập khoảng cách sự cố F21/2:", min_value=0)
