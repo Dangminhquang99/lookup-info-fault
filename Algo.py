d3555b5a564325dddf541ea0f52505c73da2200a
Dangminhquang99 <Minhquang7274@gmail.com>
2025-07-13 18:38:20 +0700

    Update Algo.py

--- Algo.py
+++ Algo.py
@@ -24,17 +24,33 @@ def findx(dis, dataframe, role_name, col_name):
     return None
 
 
-def info(dataframe, result, role_name):
+def info(df, result, role_name):
     if not result:
         st.warning(f"⚠️ Không có dữ liệu kết quả để hiển thị cho {role_name}.")
         return
-    if 'Vị trí' not in dataframe.columns:
+    if 'Vị trí' not in df.columns:
         st.error("❌ File thông tin cột thiếu cột 'Vị trí'.")
         return
     v1, v2 = result
-    selected_rows = dataframe[dataframe['Vị trí'].isin([v1, v2])]
-    if selected_rows.empty:
-        st.warning("⚠️ Không tìm thấy thông tin tương ứng trong file thông tin.")
-    else:
-        st.subheader(f"📋 Thông tin chi tiết các cột theo {role_name}:")
-        st.table(selected_rows)
+    row_index = df[df['Vị trí']==v1].index[0]
+    st.header(f"THÔNG TIN VỊ TRÍ {v1}")
+    cong_dung_cot=df.loc[row_index,'Công dụng cột']
+    st.write(f"Công dụng cột: {cong_dung_cot}")
+    Thu_tu_pha=df.loc[row_index,'Thứ tự pha']
+    st.write(f"Thứ tự pha: {Thu_tu_pha}")
+    goc_lai=df.loc[row_index,'Góc lái']
+    st.write(f"Góc lái: {goc_lai}")
+    loai_cot=df.loc[row_index,'Loại cột']
+    st.write(f"Loại cột: {loai_cot}")
+    chieu_cao_cot=df.loc[row_index,'Chiều cao cột']
+    st.write(f"Chiều cao cột: {chieu_cao_cot} m")
+    loai_tiep_dia=df.loc[row_index,'Loại tiếp địa']
+    st.write(f"Loại tiếp địa: {loai_tiep_dia}")
+    loai_day_dan=df.loc[row_index,'Loại dây dẫn']
+    st.write(f"Loại dây dẫn: {loai_day_dan}")
+    loai_cach_dien=df.loc[row_index,'Loại cách điện']
+    st.write(f"Loại cách điện: {loai_cach_dien}")
+    loai_cap_quang=df.loc[row_index,'Loại cáp quang']
+    st.write(f"Loại cáp quang: {loai_cap_quang}")
+    hanh_lang=df.loc[row_index,'Hành lang']
+    st.write(f"Hành lang: {hanh_lang}")
