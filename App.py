d6ff496904a9c8f7c9d2659fc7c77759acd703c8
Dangminhquang99 <Minhquang7274@gmail.com>
2025-07-15 21:49:12 +0700

    Update App.py

--- App.py
+++ App.py
@@ -4,41 +4,37 @@ import openpyxl as op
 import Gfile as gf
 import Algo as al
 
-#Cài đặt định dạng trang web
+# Cài đặt định dạng trang web
 st.set_page_config(page_title="Tra cứu sự cố",layout="wide",initial_sidebar_state="expanded")
-st.title("📊 TRA CỨU SỰ CỐ" )
 
-#Cấu hình đường dây
-select_dz=gf.select_name_dz()
-xem_tongke=st.checkbox("Xem tổng kê đường dây")
-tba_0, tba_1=gf.select_tba_1(select_dz)
-df = gf.accum(select_dz)
-if xem_tongke:
-    df
-col1, col2=st.columns(2)
+# Cài đặt tittle và chế độ chia cột
 
-with col1:  #TẠI TBA 1 #cột 1
-    col_name=gf.ass_col_name(tba_0)
-    st.header(f"📋-----{tba_0}------")
-    st.markdown("---")
-    dis87_1 = st.number_input(f"🔢 Nhập khoảng cách sự cố F87/1:", min_value=0)
-    dis21_1 = st.number_input(f"🔢 Nhập khoảng cách sự cố F21/1:", min_value=0)
-    result_87 = al.findx(dis87_1, df, "F87", col_name)
-    if result_87:
-        al.info(df, result_87, "F87")
-    result_21 = al.findx(dis21_1, df, "F21", col_name)
-    if result_21:
-        al.info(df, result_21, "F21")
+cola, colb=st.columns([5,1])
+with cola:
+    st.title("📊 TRA CỨU SỰ CỐ" )
+with colb:
+    view_mode = st.toggle("🔄 Chế độ chia cột")
     
-with col2:#TẠI TBA 2 #cột 2
-    col_name=gf.ass_col_name(tba_1)
-    st.header(f"📋-----{tba_1}------")
-    st.markdown("---")
-    dis87_2 = st.number_input(f"🔢 Nhập khoảng cách sự cố F87/2:", min_value=0)
-    dis21_2 = st.number_input(f"🔢 Nhập khoảng cách sự cố F21/2:", min_value=0)
-    result_87 = al.findx(dis87_2, df, "F87", col_name)
-    if result_87:
-        al.info(df, result_87, "F87")
-    result_21 = al.findx(dis21_2, df, "F21", col_name)
-    if result_21:
-        al.info(df, result_21, "F21")
+#Cấu hình đường dây, tba
+select_dz=gf.select_name_dz()
+subs_0, subs_1=gf.select_tba_1(select_dz)
+df = gf.accum(select_dz)
+
+#Chạy chương trình
+col1, col2=st.columns(2)
+st.sidebar.selectbox("Hãy chọn công việc",["Tra cứu thông tin sự cố", "Tra cứu thông tin đường dây"])
+if view_mode:
+    with col1:  #TẠI TBA 1 #cột 1
+        al.process(subs_0,df)
+    with col2:#TẠI TBA 2 #cột 2
+        al.process(subs_1,df)    
+else:
+    with st.expander(f"{subs_0}"): #TẠI TBA 1 #cột 1
+        al.process(subs_0, df)
+    with st.expander(f"{subs_1}"): #TẠI TBA 2 #cột 2
+        al.process(subs_1, df)
+
+#Xem tổng kê
+st.markdown("---")
+xem_tongke=st.checkbox("Xem tổng kê đường dây")
+if xem_tongke: df
